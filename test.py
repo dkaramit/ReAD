525a2a929ac20553bea65debc4d7723c37fb9b2e
dkaramit <dkaramit@yahoo.com>
2024-03-23 06:27:33 +0200

    topological ordering saves A LOT of memory... However, I need to avoid stack overflow errors for large expressions

--- test.py
+++ test.py
@@ -1,23 +1,86 @@
 import ReAD as rd
 
+_ONE=rd.Node(1)
+def derivatives(node):
+
+    local_derivatives={}
+
+    def topo_sort(node, visited, stack):
+        visited.add(node)
+        for child, _ in node.children:
+            if child not in visited:
+                topo_sort(child, visited, stack)
+        stack.append(node)
+
+    visited = set()
+    stack = []
+    topo_sort(node, visited, stack)
+        
+    while stack:
+        current_node = stack.pop()
+        if current_node not in local_derivatives:
+            local_derivatives[current_node] = _ONE
+
+        for child in current_node.children:
+            
+            if child[0] in local_derivatives.keys() :
+                local_derivatives[child[0]] += child[1]() * local_derivatives[current_node]
+            else:
+                local_derivatives[child[0]]  = child[1]() * local_derivatives[current_node]
+
+    return local_derivatives
 
 
 
-x = rd.Node(5)
-y = rd.Node(2)
+import psutil
+import os
+import gc
+# Get the current process
+process = psutil.Process(os.getpid())
 
-z=x+y*rd.sin(x)
-z=rd.exp(z)
+# Memory usage in bytes
+memory_use = process.memory_info().rss
+print(f"Memory start: {memory_use / (1024 * 1024):.2f} MB")
 
-dz=rd.compute_derivatives(z)
-ddz=rd.compute_derivatives(dz[x])
-print(z.value)
-print(dz[x].value,dz[y].value)
-print(ddz[x].value,ddz[y].value)
 
-values=rd.update_values(z,{x:5,y:15})
-dz=rd.compute_derivatives(z)
-ddz=rd.compute_derivatives(dz[x])
-print(z.value)
-print(dz[x].value,dz[y].value)
-print(ddz[x].value,ddz[y].value)
+
+def m1():
+    x = rd.Node(0.1)
+    y = rd.Node(0.2)
+
+    z=x+y*rd.sin(x)
+    for _ in range(50):
+        z=rd.sin(z+x*y)
+
+    dz=rd.compute_derivatives(z)
+    print(dz[x].value,dz[y].value)
+    dxdz=rd.compute_derivatives(dz[x])
+    dydz=rd.compute_derivatives(dz[y])
+    print(dxdz[x].value,dxdz[y].value)
+    print(dydz[x].value,dydz[y].value)
+    
+    memory_use = process.memory_info().rss
+    print(f"No topological order: {memory_use / (1024 * 1024):.2f} MB")
+
+def m2():
+    x = rd.Node(0.1)
+    y = rd.Node(0.2)
+
+    z=x+y*rd.sin(x)
+    for _ in range(50):
+        z=rd.sin(z+x*y)
+
+    dz=derivatives(z)
+    print(dz[x].value,dz[y].value)
+
+    dxdz=derivatives(dz[x])
+    dydz=derivatives(dz[y])
+    print(dxdz[x].value,dxdz[y].value)
+    print(dydz[x].value,dydz[y].value)
+
+    memory_use = process.memory_info().rss
+    print(f"With topological order: {memory_use / (1024 * 1024):.2f} MB")
+
+
+m1();gc.collect()
+m2()
